8ca1735c0a38740aaa37cba449f582aa777ddcf3
yano-shun <bjkhgzcl_1188ok@outlook.jp>
2025-05-13 14:27:55 +0900

    Add Render domain to ALLOWED_HOSTS

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8+sf!kw4pth8m-t*_sf+-74=d77nvwps+$m%a)z1bp7l_=ob9+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
